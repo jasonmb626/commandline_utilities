248db8b74d1a294e0c13e2b65f9952ff8a0014a7
Jason Brunelle <jasonmb626@gmail.com>
2024-07-20 08:53:05 -0500

    Add install script.

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+ln -s /home/jason/git/commandline_utilities/reencode_video_for_tablet.sh /usr/local/bin/reencode_video_for_tablet.sh 
+ln -s /home/jason/git/commandline_utilities/reencode_videos_for_tablet.sh /usr/local/bin/reencode_videos_for_tablet.sh 
--- reencode_videos_for_tablet.sh
+++ reencode_videos_for_tablet.sh
@@ -3,7 +3,7 @@
 for filename in *; do
   case $filename in
     *.mp4|*.avi|*.mkv|*.webm)
-      ./reencode_video_for_tablet.sh "$filename"
+      reencode_video_for_tablet.sh "$filename"
   esac
 done
 
