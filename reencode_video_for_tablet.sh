90a8c353b087f77c773ecbcf114a2f33828854b8
Jason Brunelle <jasonmb626@gmail.com>
2024-08-11 10:24:46 -0500

    Fix sizing and add --dry-run option. Add logging.

--- reencode_video_for_tablet.sh
+++ reencode_video_for_tablet.sh
@@ -17,12 +17,12 @@ probe_info=$(ffprobe -v error -select_streams v:0 -show_entries stream=width,hei
 orig_height=$(echo "$probe_info" | grep 'height=' | awk -F= '{print $2}')
 orig_width=$(echo "$probe_info" | grep 'width=' | awk -F= '{print $2}')
 ratio=$(echo "scale=4; $orig_width / $orig_height" | bc)
-echo "width $width height $height ratio $ratio"
+echo "orig width $orig_width height $orig_height ratio $ratio" >>conv.log
 
 new_height=720
 new_width=$(echo "scale=0; $new_height * $ratio / 1" | bc)
 
-echo "new_width $new_width new_height $new_height"
+echo "new_width $new_width new_height $new_height" >>conv.log
 
 threshold=0
 ideal_ratio=$(echo "scale=4; 1080 / $new_height" | bc)
@@ -30,11 +30,11 @@ diff=$(echo "$ideal_ratio - $ratio" | bc)
 diff="${diff/#-}"
 
 if [[ $(echo "$diff < .3" | bc) -eq 1  ]]; then
-    padded_width=1080
+    padded_width=1280
 else
-    padded_width=854
+    padded_height=720
 fi
-echo "$padded_width"
+echo "Padded width: $padded_width" >> conv.log
 
 pad_offset=0
 diff=$((padded_width - new_width))
@@ -43,7 +43,7 @@ if [[ $diff -gt 2 ]]; then
 fi
 
 new_filename=$(echo "$file_to_reencode" | sed -r 's/(.*)(.mp4|.mkv|.webm|.avi)$/\1.mp4/')
-echo "$new_filename"
+echo "$new_filename" >>conv.log
 
 cmd="ffmpeg -y -i \"$file_to_reencode\" -c:v h264 -c:a aac -ac 2 -r 30 -vf scale=$padded_width:$new_height \"conv/$new_filename\""
 if [[ $pad_offset -ne 0 ]]; then
@@ -53,5 +53,8 @@ fi
 if [[ ! -d conv ]]; then
     mkdir conv
 fi
-echo "Running $cmd"
-eval $cmd
+echo "Running $cmd" >>conv.log
+
+if [[ "$2" != "--dry-run" ]]; then
+    eval $cmd
+fi
--- reencode_videos_for_tablet.sh
+++ reencode_videos_for_tablet.sh
@@ -3,7 +3,7 @@
 for filename in *; do
   case $filename in
     *.mp4|*.avi|*.mkv|*.webm)
-      reencode_video_for_tablet.sh "$filename"
+      reencode_video_for_tablet.sh "$filename" $1 #pass --dry-run to this script in order to also pass that along to the script
   esac
 done
 
